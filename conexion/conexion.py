8f019ccf4dac8c9c11f6f4245e86903b4c8306c4
LuciaBravoUEA <lc.bravoo@uea.edu.ec>
2026-03-15 12:33:23 -0500

    Add files via upload

--- conexion/conexion.py
+++ conexion/conexion.py
@@ -1,28 +1,15 @@
-import os
 import mysql.connector
-from urllib.parse import urlparse
 
 def conectar():
     try:
-        db_url = os.getenv("DATABASE_URL")
-
-        # Cambiar mysql:// por mysql+mysqlconnector://
-        if db_url.startswith("mysql://"):
-            db_url = db_url.replace("mysql://", "mysql+mysqlconnector://", 1)
-
-        url = urlparse(db_url)
-
         conexion = mysql.connector.connect(
-            host=url.hostname,
-            user=url.username,
-            password=url.password,
-            database=url.path.replace("/", ""),
-            port=url.port
+            host="b6yidpnlrwoyrb2wge9z-mysql.services.clever-cloud.com",
+            user="USUARIO",
+            password="CONTRASEÑA",
+            database="b6yidpnlrwoyrb2wge9z",
+            port=3306
         )
-
-        print("Conexión exitosa")
         return conexion
-
     except Exception as e:
         print("Error conectando a la base de datos:", e)
         return None
